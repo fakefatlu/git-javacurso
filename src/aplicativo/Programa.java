daa82c25819c9da44980026b022c12038c5a8792
fakefatlu <luana199835@hotmail.com>
2020-03-21 10:46:20 -0300

    Dados do cliente

--- src/aplicativo/Programa.java
+++ src/aplicativo/Programa.java
@@ -9,14 +9,24 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 		
 		String nome;
+		String cidade;
+		
+		int idade;
 	
 	
 	 System.out.println("Olá, bom dia meu amigo.");
 	 System.out.println("Qual o seu nome: ");
 	 nome = sc.nextLine();
-	 
-	 
+	 System.out.println("Idade: ");
+	 idade = sc.nextInt();
+	 sc.nextLine();
+	 System.out.println("Cidade natal: ");
+	 cidade = sc.nextLine(); 
+
+	 System.out.println("-----------------------------");
 	 System.out.println("Nome do cliente: " + nome);
+	 System.out.println("Idade: " + idade + " anos");
+	 System.out.println("Cidade natal: " + cidade);
 	 
 	sc.close();
 	}
