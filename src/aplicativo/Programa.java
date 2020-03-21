12340bbd57a7d3d2dcc7340133a6cd46e4981569
fakefatlu <luana199835@hotmail.com>
2020-03-21 10:28:58 -0300

    Nome do cliente

--- src/aplicativo/Programa.java
+++ src/aplicativo/Programa.java
@@ -1,10 +1,24 @@
 package aplicativo;
 
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+	
+	
 	 System.out.println("Olá, bom dia meu amigo.");
+	 System.out.println("Qual o seu nome: ");
+	 nome = sc.nextLine();
+	 
+	 
+	 System.out.println("Nome do cliente: " + nome);
+	 
+	sc.close();
 	}
 
 }
